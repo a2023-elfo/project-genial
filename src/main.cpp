045360e462659236c9d59f395eb2fc18baa341f1
Jordan Gagnon <itsseraphii@gmail.com>
2023-11-07 15:50:43 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+
+void setup() {
+  BoardInit();
+  Serial.begin(9600);
+  Serial.println("Hello World!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
