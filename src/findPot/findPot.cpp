3bd400c8da7ddc996acd0f25158c0194dff81c14
Olivier Pinard <olivierp3107@icloud.com>
2023-11-13 22:45:45 -0500

    Robot trouve pot

--- src/findPot/findPot.cpp
+++ src/findPot/findPot.cpp
@@ -1,6 +1,7 @@
 #include <findPot/findPot.h>
 
 int* _outputFP = 0;
+int not_found = 0;
 static const int TROUVER_LIGNE = 0;
 static const int SUIVRE_LIGNE = 1;
 static const int TROUVER_POT = 2;
@@ -11,12 +12,52 @@ void findPotsetup(int* output){
     _outputFP = output;
 };
 
-
-void findPotloop(){
-     Serial.println("je suis dans trouver pot");
-    if(ROBUS_IsBumper(3)){
-        *_outputFP = ARROSER;
-    }    
+void findPotloop()
+{
+    Serial.println("je suis dans trouver pot");
+    ENCODER_Reset(LEFT);
+    ENCODER_Reset(RIGHT);
+    
+    while (ROBUS_ReadIR(0) < 200)
+    {
+        if (ENCODER_Read(RIGHT) > 2500)
+        {
+            not_found = 1;
+            break;
+        }
+        MOTOR_SetSpeed(LEFT, -0.2);
+        MOTOR_SetSpeed(RIGHT, 0.2);
+    }
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
+    delay(500);
+    if (not_found == 1)
+    {
+        ENCODER_Reset(LEFT);
+        while (ENCODER_Read(LEFT) < 2500)
+        {
+            MOTOR_SetSpeed(LEFT, 0.2);
+            MOTOR_SetSpeed(RIGHT, -0.2);
+        }
+        
+        while (ROBUS_ReadIR(0) < 200)
+        {
+            MOTOR_SetSpeed(LEFT, 0.2);
+            MOTOR_SetSpeed(RIGHT, -0.2);
+        }
+    }
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
+    delay(500);
+    not_found = 0;
+    while (ROBUS_ReadIR(0) < 575)
+    {
+        MOTOR_SetSpeed(LEFT, 0.2);
+        MOTOR_SetSpeed(RIGHT, 0.2);
+    }
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
+    *_outputFP = ARROSER;
 };
 
 
